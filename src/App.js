98930f097427417dcbe952bf5793b518a2f67778
johnnybasgallop <johnnybasgallopwork@outlook.com>
2024-12-28 17:42:57 -0800

    changed some folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Navigation/Navbar/components/Navbar";
+import Navbar from "./Navbar/Navbar";
 
 function App() {
   return (
