ce2b6108b51c32f3b1e04b6e96f9eaae2f21f741
johnnybasgallop <johnnybasgallopwork@outlook.com>
2024-12-29 19:25:11 -0800

    blanl

--- src/Landing/HeroSection.jsx
+++ src/Landing/HeroSection.jsx
@@ -21,9 +21,9 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <section className="flex items-center justify-between px-[20px] md:px-[30px] lg:px-[40px] xl:px-[80px] py-12 lg:py-[80px] xl:py-[120px] 2xl:py-[150px] relative">
+    <section className="relative flex items-center justify-between px-[20px] md:px-[30px] lg:px-[40px] xl:px-[80px] py-12 lg:py-[80px] xl:py-[120px] 2xl:py-[150px]">
       {/* Left Content */}
-      <div className="xl:w-[50%] w-[65%] md:w-[50%] lg:w-[60%] space-y-6 flex flex-col items-start justify-start text-left z-10">
+      <div className="w-[65%] md:w-[50%] lg:w-[60%] xl:w-[50%] flex flex-col items-start justify-start space-y-6 text-left z-10">
         {/* Heading */}
         <h1 className="text-4xl md:text-6xl 2xl:text-[70px] font-extrabold leading-snug xl:leading-normal">
           Building Wealth with{" "}
@@ -34,7 +34,7 @@ const HeroSection = () => {
         </h1>
 
         {/* Subtext */}
-        <p className="text-gray-600 text-[16px] pr-3 md:text-lg 2xl:text-xl leading-normal pb-2 md:pb-10 md:w-[80%]">
+        <p className="text-[16px] md:text-lg 2xl:text-xl text-gray-600 leading-normal pb-2 md:pb-10 pr-3 md:w-[80%]">
           Fairway offers unique investment solutions, combining sophisticated
           algorithms with human intelligence to deliver robust capital growth.
           Our advanced strategies give our clients a competitive edge, in all
@@ -42,16 +42,16 @@ const HeroSection = () => {
         </p>
 
         {/* Email Input Section */}
-        <div className="space-y-4 md:space-y-8 w-full">
+        <div className="w-full space-y-4 md:space-y-8">
           <p className="text-green-600 font-medium">Want to learn more?</p>
-          <button className="bg-green-600 text-white px-8 py-3 lg:text-md 2xl:px-12 2xl:py-4 2xl:text-lg font-medium hover:bg-green-700 rounded-full">
+          <button className="px-8 py-3 2xl:px-12 2xl:py-4 bg-green-600 text-white rounded-full font-medium lg:text-md 2xl:text-lg hover:bg-green-700">
             Get Started
           </button>
         </div>
       </div>
 
       {/* Right Diagonal Image */}
-      <div className=" absolute top-0 right-0 w-[40%] md:w-[50%] h-[90%] py-12 md:py-0">
+      <div className="absolute top-0 right-0 h-[90%] w-[40%] md:w-[50%] py-12 md:py-0">
         <div
           className="w-full h-full bg-cover bg-center"
           style={{
