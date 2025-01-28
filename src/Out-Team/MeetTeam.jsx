aa90b9e0769867daea1e8a941a5a4a59a5863c4d
johnnybasgallop <johnnybasgallopwork@outlook.com>
2025-01-28 14:35:47 +0000

    fixed popups, still needs md and sm sizing adjustments

--- src/Out-Team/MeetTeam.jsx
+++ src/Out-Team/MeetTeam.jsx
@@ -10,7 +10,7 @@ function MeetTeam() {
       name: "Oliver Bean",
       title: "Example title",
       description:
-        "efuewnfioewnfioewnfioewnfoiewnfionewiofnweoinfoin io nfoiew nfioe wnfio enwfio newn feonw foienw foinewi fio new iofn oiewnf ioewnf inewio fnew ifnew iofoewnf ionew ofinew fewno finwioneiof neiwon ne inewoi nnw'NFIOEnfioenoew ifnflnewklfnfioweoifnweifnowefn",
+        "Oliver Bean is a seasoned real estate professional specializing in portfolio management, investment strategies, and value driven renovations. With extensive experience in both Britain’s and Dubai's dynamic markets. He delivers tailored solutions to help clients build, expand, and optimise their real estate holdings. Beyond real estate Oliver has a very strong network of clients he  has worked with over the years across an array of industries and has established himself as a reliable and trusted advisor to many. His attention to detail ensures he stays ahead of market trends, constantly refining his strategies providing his clients with up-to-date market insights and innovative solutions. Oliver has always has a desire to step into the industry and believes that at Fairway Capital he has a winning formula with the team of founders all possessing individual qualities that allow the company to flourish in years to come from solid foundations. He has taken on his role as CEO of the company in stride and has implemented procedures and processes to streamline productivity in all areas of the business. As well as oversight of the whole business I am always on hand for existing and potential clients to discuss all things Fairway Capital, clients, helping them generate substantial trading revenues through his disciplined and innovative approach. His ability to consistently deliver on both individual and institutional levels reflects his deep understanding of market dynamics and risk management. With over 700 hours of live-streamed trading and mentoring, Alfie has shared his proprietary strategies with a global audience, building trust and inspiring traders to adopt a structured, results-focused mindset. His leadership and hands-on approach have been pivotal in fostering the firm’s growth and maintaining its reputation for excellence. Alfie’s experience spans the full spectrum of trading operations, from developing innovative strategies to overseeing day-to-day market execution. His disciplined approach to risk management and deep understanding of market dynamics ensure consistent results that align with client objectives and long-term financial goals.  lf as a reliable and trusted advisor to many. His attention to detail ensures he stays ahead of market trends, constantly refining his strategies providing his clients with up-to-date market insights and innovative solutions. Oliver has always has a desire to step into the industry and believes that at Fairway Capital he has a winning formula with the team of founders all possessing individual qualities that allow the company to flourish in years to come from solid foundations. He has taken on his role as CEO of the company in stride and has implemented procedures and processes to streamline productivity in all areas of the business. As well as oversight of the whole business I am always on hand for existing and potential clients to discuss all things Fairway Capital, clients, helping them generate substantial trading revenues through his disciplined and innovative approach. His ability to consistently deliver on both individual and institutional levels reflects his deep understanding of market dynamics and risk management. With over 700 hours of live-streamed trading and mentoring, Alfie has shared his proprietary strategies with a global audience, building trust and inspiring traders to adopt a structured, results-focused mindset. His leadership and hands-on approach have been pivotal in fostering the firm’s growth and maintaining its reputation for excellence. Alfie’s experience spans the full spectrum of trading operations, from developing innovative strategies to overseeing day-to-day market execution. His disciplined approach to risk management and deep understanding of market dynamics ensure consistent results that align with client objectives and long-term financial goals. ",
       image: "../ollie-ss.png", // Replace with actual image path
       bgColor: "bg-[#EAF6E7]", // Light Green
     },
@@ -18,7 +18,7 @@ function MeetTeam() {
       name: "Oliver Bean",
       title: "Example title",
       description:
-        "efuewnfioewnfioewnfioewnfoiewnfionewiofnweoinfoin io nfoiew nfioe wnfio enwfio newn feonw foienw foinewi fio new iofn oiewnf ioewnf inewio fnew ifnew iofoewnf ionew ofinew fewno finwioneiof neiwon ne inewoi nnw'NFIOEnfioenoew ifnflnewklfnfioweoifnweifnowefn",
+        "Oliver Bean is a seasoned real estate professional specializing in portfolio management, investment strategies, and value driven renovations. With extensive experience in both Britain’s and Dubai's dynamic markets. He delivers tailored solutions to help clients build, expand, and optimise their real estate holdings. Beyond real estate Oliver has a very strong network of clients he  has worked with over the years across an array of industries and has established himself as a reliable and trusted advisor to many. His attention to detail ensures he stays ahead of market trends, constantly refining his strategies providing his clients with up-to-date market insights and innovative solutions. Oliver has always has a desire to step into the industry and believes that at Fairway Capital he has a winning formula with the team of founders all possessing individual qualities that allow the company to flourish in years to come from solid foundations. He has taken on his role as CEO of the company in stride and has implemented procedures and processes to streamline productivity in all areas of the business. As well as oversight of the whole business I am always on hand for existing and potential clients to discuss all things Fairway Capital, clients, helping them generate substantial trading revenues through his disciplined and innovative approach. His ability to consistently deliver on both individual and institutional levels reflects his deep understanding of market dynamics and risk management. With over 700 hours of live-streamed trading and mentoring, Alfie has shared his proprietary strategies with a global audience, building trust and inspiring traders to adopt a structured, results-focused mindset. His leadership and hands-on approach have been pivotal in fostering the firm’s growth and maintaining its reputation for excellence. Alfie’s experience spans the full spectrum of trading operations, from developing innovative strategies to overseeing day-to-day market execution. His disciplined approach to risk management and deep understanding of market dynamics ensure consistent results that align with client objectives and long-term financial goals.",
       image: "../ollie-ss.png", // Replace with actual image path
       bgColor: "bg-[#D7E8FF]", // Light Blue
     },
@@ -26,7 +26,7 @@ function MeetTeam() {
       name: "Oliver Bean",
       title: "Example title",
       description:
-        "efuewnfioewnfioewnfioewnfoiewnfionewiofnweoinfoin io nfoiew nfioe wnfio enwfio newn feonw foienw foinewi fio new iofn oiewnf ioewnf inewio fnew ifnew iofoewnf ionew ofinew fewno finwioneiof neiwon ne inewoi nnw'NFIOEnfioenoew ifnflnewklfnfioweoifnweifnowefn",
+        "Oliver Bean is a seasoned real estate professional specializing in portfolio management, investment strategies, and value driven renovations. With extensive experience in both Britain’s and Dubai's dynamic markets. He delivers tailored solutions to help clients build, expand, and optimise their real estate holdings. Beyond real estate Oliver has a very strong network of clients he  has worked with over the years across an array of industries and has established himself as a reliable and trusted advisor to many. His attention to detail ensures he stays ahead of market trends, constantly refining his strategies providing his clients with up-to-date market insights and innovative solutions. Oliver has always has a desire to step into the industry and believes that at Fairway Capital he has a winning formula with the team of founders all possessing individual qualities that allow the company to flourish in years to come from solid foundations. He has taken on his role as CEO of the company in stride and has implemented procedures and processes to streamline productivity in all areas of the business. As well as oversight of the whole business I am always on hand for existing and potential clients to discuss all things Fairway Capital, clients, helping them generate substantial trading revenues through his disciplined and innovative approach. His ability to consistently deliver on both individual and institutional levels reflects his deep understanding of market dynamics and risk management. With over 700 hours of live-streamed trading and mentoring, Alfie has shared his proprietary strategies with a global audience, building trust and inspiring traders to adopt a structured, results-focused mindset. His leadership and hands-on approach have been pivotal in fostering the firm’s growth and maintaining its reputation for excellence. Alfie’s experience spans the full spectrum of trading operations, from developing innovative strategies to overseeing day-to-day market execution. His disciplined approach to risk management and deep understanding of market dynamics ensure consistent results that align with client objectives and long-term financial goals.",
       image: "../ollie-ss.png", // Replace with actual image path
       bgColor: "bg-[#FFF9D7]", // Pale Yellow
     },
@@ -34,7 +34,7 @@ function MeetTeam() {
       name: "Oliver Bean",
       title: "Example title",
       description:
-        "efuewnfioewnfioewnfioewnfoiewnfionewiofnweoinfoin io nfoiew nfioe wnfio enwfio newn feonw foienw foinewi fio new iofn oiewnf ioewnf inewio fnew ifnew iofoewnf ionew ofinew fewno finwioneiof neiwon ne inewoi nnw'NFIOEnfioenoew ifnflnewklfnfioweoifnweifnowefn",
+        "Oliver Bean is a seasoned real estate professional specializing in portfolio management, investment strategies, and value driven renovations. With extensive experience in both Britain’s and Dubai's dynamic markets. He delivers tailored solutions to help clients build, expand, and optimise their real estate holdings. Beyond real estate Oliver has a very strong network of clients he  has worked with over the years across an array of industries and has established himself as a reliable and trusted advisor to many. His attention to detail ensures he stays ahead of market trends, constantly refining his strategies providing his clients with up-to-date market insights and innovative solutions. Oliver has always has a desire to step into the industry and believes that at Fairway Capital he has a winning formula with the team of founders all possessing individual qualities that allow the company to flourish in years to come from solid foundations. He has taken on his role as CEO of the company in stride and has implemented procedures and processes to streamline productivity in all areas of the business. As well as oversight of the whole business I am always on hand for existing and potential clients to discuss all things Fairway Capital, clients, helping them generate substantial trading revenues through his disciplined and innovative approach. His ability to consistently deliver on both individual and institutional levels reflects his deep understanding of market dynamics and risk management. With over 700 hours of live-streamed trading and mentoring, Alfie has shared his proprietary strategies with a global audience, building trust and inspiring traders to adopt a structured, results-focused mindset. His leadership and hands-on approach have been pivotal in fostering the firm’s growth and maintaining its reputation for excellence. Alfie’s experience spans the full spectrum of trading operations, from developing innovative strategies to overseeing day-to-day market execution. His disciplined approach to risk management and deep understanding of market dynamics ensure consistent results that align with client objectives and long-term financial goals.",
       image: "../ollie-ss.png", // Replace with actual image path
       bgColor: "bg-[#EAE3FF]", // Lavender
     },
@@ -42,7 +42,7 @@ function MeetTeam() {
       name: "Oliver Bean",
       title: "Example title",
       description:
-        "efuewnfioewnfioewnfioewnfoiewnfionewiofnweoinfoin io nfoiew nfioe wnfio enwfio newn feonw foienw foinewi fio new iofn oiewnf ioewnf inewio fnew ifnew iofoewnf ionew ofinew fewno finwioneiof neiwon ne inewoi nnw'NFIOEnfioenoew ifnflnewklfnfioweoifnweifnowefn",
+        "Oliver Bean is a seasoned real estate professional specializing in portfolio management, investment strategies, and value driven renovations. With extensive experience in both Britain’s and Dubai's dynamic markets. He delivers tailored solutions to help clients build, expand, and optimise their real estate holdings. Beyond real estate Oliver has a very strong network of clients he  has worked with over the years across an array of industries and has established himself as a reliable and trusted advisor to many. His attention to detail ensures he stays ahead of market trends, constantly refining his strategies providing his clients with up-to-date market insights and innovative solutions. Oliver has always has a desire to step into the industry and believes that at Fairway Capital he has a winning formula with the team of founders all possessing individual qualities that allow the company to flourish in years to come from solid foundations. He has taken on his role as CEO of the company in stride and has implemented procedures and processes to streamline productivity in all areas of the business. As well as oversight of the whole business I am always on hand for existing and potential clients to discuss all things Fairway Capital, clients, helping them generate substantial trading revenues through his disciplined and innovative approach. His ability to consistently deliver on both individual and institutional levels reflects his deep understanding of market dynamics and risk management. With over 700 hours of live-streamed trading and mentoring, Alfie has shared his proprietary strategies with a global audience, building trust and inspiring traders to adopt a structured, results-focused mindset. His leadership and hands-on approach have been pivotal in fostering the firm’s growth and maintaining its reputation for excellence. Alfie’s experience spans the full spectrum of trading operations, from developing innovative strategies to overseeing day-to-day market execution. His disciplined approach to risk management and deep understanding of market dynamics ensure consistent results that align with client objectives and long-term financial goals.",
       image: "../ollie-ss.png", // Replace with actual image path
       bgColor: "bg-[#EAE3FF]", // Lavender
     },
@@ -134,24 +134,50 @@ function MeetTeam() {
             className="absolute inset-0 bg-gray-900 opacity-50"
             onClick={closeModal}
           ></div>
-          <div className="bg-white rounded-lg z-10 max-w-[50%] w-full mx-4">
-            <div className="p-6">
-              <img
-                className={`${selectedMember.bgColor} w-40 h-40 rounded-lg text-left`}
-                src={ollieImage}
-                alt={selectedMember.name}
-              />
-              <h4 className="text-2xl font-semibold text-gray-800 mt-4 text-center">
-                {selectedMember.name}
-              </h4>
-              <p className="mt-2 text-gray-600">{selectedMember.description}</p>
-              <div className="mt-6 flex justify-end">
-                <button
-                  className="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded-md"
-                  onClick={closeModal}
-                >
-                  Close
-                </button>
+          <div className="bg-white rounded-lg z-10 max-w-4xl w-full mx-4 px-[10px] overflow-hidden">
+            <div className="flex p-6 gap-6">
+              {/* Image Section */}
+
+              {/* Content Section */}
+              <div className="w-full max-h-[500px] overflow-y-auto">
+                <div className="w-full flex flex-row items-center space-x-6 mb-10">
+                  <div
+                    className={`${selectedMember.bgColor} w-[15%] rounded-lg p-2 flex items-center justify-center`}
+                  >
+                    <img
+                      src={ollieImage}
+                      alt={selectedMember.name}
+                      className="w-full h-auto object-cover"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <h4 className="text-2xl font-semibold text-gray-800">
+                      {selectedMember.name}
+                    </h4>
+                    <h5 className="text-lg text-gray-500 mt-2">
+                      {selectedMember.title}
+                    </h5>
+                  </div>
+                  <div className="px-2 flex ">
+                    <button
+                      className="px-6 py-2 bg-[#2A5C6D] hover:bg-[#1a4656] text-white rounded-md transition-colors"
+                      onClick={closeModal}
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+
+                {/* Description with Paragraphs */}
+                <div className="mt-4">
+                  {selectedMember.description
+                    .split("\n\n")
+                    .map((paragraph, index) => (
+                      <p key={index} className="mb-4 text-gray-600">
+                        {paragraph}
+                      </p>
+                    ))}
+                </div>
               </div>
             </div>
           </div>
