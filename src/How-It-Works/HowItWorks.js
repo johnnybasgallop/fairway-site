5f488ddea8af497ea931069fced46299e607d4ca
johnnybasgallop <johnnybasgallopwork@outlook.com>
2025-02-05 18:32:38 +0000

    styled the how it works main bt

--- src/How-It-Works/HowItWorks.js
+++ src/How-It-Works/HowItWorks.js
@@ -10,9 +10,9 @@ export default function HowItWorks() {
         How it works
       </h2>
 
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-[50px] flex justify-between bg-gray-100 py-8 px-[50px] rounded-lg">
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 gap-[50px] flex bg-gray-50 py-8 px-[30px] rounded-lg">
         {/* Basics Section */}
-        <div className="flex flex-grow flex-col">
+        <div className="flex flex-1 flex-col">
           <h2 className="text-4xl font-bold py-10">The Basics.</h2>
           <p className="mt-4 text-gray-700">
             We meticulously track and analyze all trading activity, creating a
@@ -33,16 +33,16 @@ export default function HowItWorks() {
         </div>
 
         {/* Details Section */}
-        <div className="w-[80%] flex flex-col">
+        <div className="flex flex-2 flex-col">
           <h2 className="text-4xl py-10 flex items-center font-bold">
             The Details.
           </h2>
-          <div className="space-y-6 border-gray-300">
+          <div className="space-y-6 flex flex-col justify-center border-gray-300">
             {/* Data Tracking */}
-            <div className="flex items-start justify-between py-4 border-b border-gray-300">
-              <div className="flex flex-col justify-center mb-2">
+            <div className="flex items-start justify-between pb-2 pt-4 border-b border-gray-300">
+              <div className="flex flex-col justify-center mb-4">
                 <h3 className="font-bold">Data tracking and analysis</h3>
-                <p className="text-gray-500 text-sm">
+                <p className="text-gray-500 text-sm max-w-[90%] md:max-w-[100%]">
                   View how we collect and use our data
                 </p>
               </div>
@@ -52,7 +52,7 @@ export default function HowItWorks() {
             </div>
 
             {/* What Goes Into a Trade */}
-            <div className="flex items-center justify-between py-4 border-b border-gray-300">
+            <div className="flex items-center justify-between pb-6 border-b border-gray-300">
               <div className="flex flex-col items-start justify-start mb-2">
                 <h3 className="font-bold">What goes into a trade</h3>
                 <p className="text-gray-500 text-sm">Discover our process</p>
@@ -63,10 +63,10 @@ export default function HowItWorks() {
             </div>
 
             {/* Technical Analysis */}
-            <div className="flex items-center justify-between py-4">
+            <div className="flex items-center justify-between pt-2 pb-4">
               <div className="flex flex-col items-start justify-start mb-2">
                 <h3 className="font-bold">Technical Analysis</h3>
-                <p className="text-gray-500 text-sm">
+                <p className="text-gray-500 text-sm max-w-[90%] md:max-w-[100%]">
                   Learn about our one-of-a-kind system
                 </p>
               </div>
